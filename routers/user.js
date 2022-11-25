a2b6cfbe2323be516a0597eca1a370e3681d8eb6
123Ksl <361617463@qq.com>
2022-11-25 11:01:06 +0800

    ddd

--- routers/user.js
+++ routers/user.js
@@ -14,12 +14,76 @@ const mysql = require('mysql');
 
 
 
-router.post('/register', async (req, res, next) => {
+router.post('/tablePost', async (req, res, next) => {
   // const checkUsername = `INSERT INTO products VALUES (1, 'Cheese', 9.99);`;
-  const sql=`SELECT * FROM table1 , table2;`
+  // console.log(req.body)
+  const count = Number(req.body.count)
+  const name=String(req.body.name)
+  const sql=`INSERT INTO table1 VALUES (${count},'${name}')`
+  console.log(sql)
   if(global.client){
     global.client.query(sql).then(_=>{
       console.log(_.rows)
+      res.send({ status: 200, data: _.rows, token: 'token' });
+    })
+    .catch(error=>{
+      res.send({ status: 400, data: error, token: 'token' });
+    })
+  }else{
+    console.log('no')
+  }
+  
+  // con.query(name, [username], (err, result, fields) => {
+  //   console.log(result)
+  //   res.send({ status: 1, data: req.body, token: 'token' });
+  // })
+  // try {
+  //   const checkUsername = `INSERT INTO products VALUES (1, 'Cheese', 9.99);`;
+  //   con.query(checkUsername, [username], (err, result, fields) => {
+  //     console.log(result)
+  //     res.send({ status: 1, data: req.body, token: 'token' });
+  //   })
+
+  // } catch (error) {
+  //   res.send({ status: 0, error: error });
+  // }
+  // try {
+  //   let { username, email, password } = req.body; 
+
+  //   const hashed_password = md5(password.toString())
+
+  //   const checkUsername = `Select username FROM users WHERE username = ?`;
+  //   con.query(checkUsername, [username], (err, result, fields) => {
+  //     if(!result.length){
+  //       const sql = `Insert Into users (username, email, password) VALUES ( ?, ?, ? )`
+  //       con.query(
+  //         sql, [username, email, hashed_password],
+  //       (err, result, fields) =>{
+  //         if(err){
+  //           res.send({ status: 0, data: err });
+  //         }else{
+  //           let token = jwt.sign({ data: result }, 'secret')
+  //           res.send({ status: 1, data: result, token : token });
+  //         }
+
+  //       })
+  //     }
+  //   });
+
+
+
+
+  // } catch (error) {
+  //   res.send({ status: 0, error: error });
+  // }
+});
+router.get('/tableGet', async (req, res, next) => {
+  // const checkUsername = `INSERT INTO products VALUES (1, 'Cheese', 9.99);`;
+  const sql=`SELECT * FROM table2 , table1;`
+  if(global.client){
+    console.log(sql)
+    global.client.query(sql).then(_=>{
+      console.log(_)
       res.send({ status: 200, data: _.rows, token: 'token' });
     })
     .catch(error=>{
