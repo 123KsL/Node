9c574873109043c9989b447693c10cf870c3c350
123Ksl <361617463@qq.com>
2024-09-03 14:05:52 +0800

    ddd

--- createNotepad.js
+++ createNotepad.js
@@ -1,20 +1,53 @@
+// const { exec } = require('child_process');
+// const path = require('path');
+
+// const date = new Date().toISOString().split('T')[0];
+// const filePath = path.join(require('os').homedir(), 'Desktop', `${date}.txt`);
+
+// // 检查文件是否存在并创建
+// exec(`powershell -Command "if (-Not (Test-Path '${filePath}')) { New-Item -Path '${filePath}' -ItemType File }"`, (err, stdout, stderr) => {
+//     if (err) {
+//         console.error(`Error creating file: ${err}`);
+//         return;
+//     }
+
+//     // 打开文件
+//     exec(`powershell -Command "Start-Process notepad '${filePath}'"`, (err, stdout, stderr) => {
+//         if (err) {
+//             console.error(`Error opening file: ${err}`);
+//         }
+//     });
+// });
+
 const { exec } = require('child_process');
 const path = require('path');
+const os = require('os');
 
 const date = new Date().toISOString().split('T')[0];
-const filePath = path.join(require('os').homedir(), 'Desktop', `${date}.txt`);
+const desktopDir = path.join(os.homedir(), 'Desktop');
+const folderPath = path.join(desktopDir, 'Notepads');
+const filePath = path.join(folderPath, `${date}.txt`);
 
-// 检查文件是否存在并创建
-exec(`powershell -Command "if (-Not (Test-Path '${filePath}')) { New-Item -Path '${filePath}' -ItemType File }"`, (err, stdout, stderr) => {
+// 创建 Notepads 文件夹（如果不存在）
+exec(`powershell -Command "if (-Not (Test-Path '${folderPath}')) { New-Item -Path '${folderPath}' -ItemType Directory }"`, (err) => {
     if (err) {
-        console.error(`Error creating file: ${err}`);
+        console.error(`Error creating folder: ${err}`);
         return;
     }
 
-    // 打开文件
-    exec(`powershell -Command "Start-Process notepad '${filePath}'"`, (err, stdout, stderr) => {
+    // 创建文件（如果不存在）
+    exec(`powershell -Command "if (-Not (Test-Path '${filePath}')) { New-Item -Path '${filePath}' -ItemType File }"`, (err) => {
         if (err) {
-            console.error(`Error opening file: ${err}`);
+            console.error(`Error creating file: ${err}`);
+            return;
         }
+
+        // 打开文件
+        exec(`powershell -Command "Start-Process notepad '${filePath}'"`, (err) => {
+            if (err) {
+                console.error(`Error opening file: ${err}`);
+            }
+        });
     });
 });
+
